2d9637bf27068fc3312e7ea3af6b6b8d1a976662
Jordan Yu <jordanyu44@gmail.com>
2018-09-04 11:40:21 -0700

    Complete dynamic array class

--- lib/dynamic_array.rb
+++ lib/dynamic_array.rb
@@ -4,10 +4,9 @@ class DynamicArray
   attr_reader :length
 
   def initialize
-    @capacity = 4
+    @capacity = 8
     @length = 0 
     @store = StaticArray.new(@capacity)
-    @start = nil
   end
 
   # O(1)
@@ -20,8 +19,7 @@ class DynamicArray
   def []=(index, value)
     check_index(index)
     store[index] = value
-    length += 1
-    start = start || 0
+    @length += 1
   end
 
   # O(1)
@@ -43,14 +41,26 @@ class DynamicArray
   # O(n): has to shift over all the elements.
   def shift
     check_index(0)
+    val = store[0]
+    1.upto(length - 1) do |idx|
+      store[idx - 1] = store[idx]
+    end
+    @length -= 1
+    val
   end
-
+  
   # O(n): has to shift over all the elements.
   def unshift(val)
+    resize! if length == capacity 
+    (length).downto(1) do |idx|
+      store[idx] = store[idx - 1]
+    end
+    store[0] = val;
+    @length += 1
   end
 
   protected
-  attr_accessor :capacity, :store, :start 
+  attr_accessor :capacity, :store 
   attr_writer :length
 
   def check_index(index)
@@ -59,5 +69,9 @@ class DynamicArray
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
+    @capacity *= 2
+    new_store = StaticArray.new(capacity)
+    0.upto(length) {|idx| new_store[idx] = store[idx]}
+    @store = new_store
   end
 end
